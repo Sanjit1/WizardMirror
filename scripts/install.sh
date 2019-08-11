22ca60a3c6162f5e85cb02224991a82931c15265
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-08-10 22:33:14 -0700

    remove cecho

--- scripts/install.sh
+++ scripts/install.sh
@@ -87,15 +87,3 @@ xdg-open README.md
 
 cd ~/MagicMirror
 
-
-
-
-cecho(){
-    RED="\033[0;31m"
-    GREEN="\033[0;32m"
-    YELLOW="\033[1;33m"
-    # ... ADD MORE COLORS
-    NC="\033[0m" # No Color
-
-    printf "${!1}${2} ${NC}\n"
-}
